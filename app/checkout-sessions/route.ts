5ebc86ce4ce1fa975e30d9c9a7b03bee02d8a3c5
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2023-07-01 17:51:45 +0800

    Add try-catch block

--- app/checkout-sessions/route.ts
+++ app/checkout-sessions/route.ts
@@ -1,5 +1,6 @@
 import { stripe } from "@/lib/stripe";
 import { NextResponse } from "next/server";
+import Stripe from "stripe";
 
 export interface CheckoutSubscriptionBody {
   plan: string;
@@ -11,27 +12,34 @@ export interface CheckoutSubscriptionBody {
 export async function POST(req: Request) {
   const body = (await req.json()) as CheckoutSubscriptionBody;
   const origin = req.headers.get("origin") || "http://localhost:3000";
-  const session = await stripe.checkout.sessions.create({
-    mode: "subscription",
-    line_items: [
-      {
-        price_data: {
-          currency: "usd",
-          recurring: {
-            interval: body.interval,
-            interval_count: 1,
+  try {
+    const session = await stripe.checkout.sessions.create({
+      mode: "subscription",
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            recurring: {
+              interval: body.interval,
+              interval_count: 1,
+            },
+            unit_amount: body.amount,
+            product_data: {
+              name: body.plan,
+              description: body.planDescription,
+            },
           },
-          unit_amount: body.amount,
-          product_data: {
-            name: body.plan,
-            description: body.planDescription,
-          },
+          quantity: 1,
         },
-        quantity: 1,
-      },
-    ],
-    success_url: `${origin}/signup?session_id={CHECKOUT_SESSION_ID}`,
-    cancel_url: `${origin}/cancel?session_id={CHECKOUT_SESSION_ID}`,
-  });
-  return NextResponse.json(session);
+      ],
+      success_url: `${origin}/signup?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${origin}/cancel?session_id={CHECKOUT_SESSION_ID}`,
+    });
+    return NextResponse.json(session);
+  } catch (error) {
+    if (error instanceof Stripe.errors.StripeError) {
+      const { message } = error;
+      return NextResponse.json({ message }, { status: error.statusCode });
+    }
+  }
 }
